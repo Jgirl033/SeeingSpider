6d467318a6058f3cdfd9ab35e7bb68e52af7d30b
Jgirl033 <18819423747@163.com>
2017-04-12 10:07:00 +0800

    Created for the static statements of the project.

---
+++ src/common/constant/Constant.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package common.constant;
+
+import org.openqa.selenium.WebDriver;
+
+
+/**
+ * 全局变量类
+ *
+ * @author admin Jgirl
+ */
+public class Constant {
+
+    /**
+     * 生成豆瓣对应的WebDriver用于登录豆瓣账号
+     */
+    public static WebDriver doubanDR;
+
+    /**
+     * 生成时光网对应的WebDriver用于登录时光网账号
+     */
+    public static WebDriver mtimeDR;
+
+    
+}
