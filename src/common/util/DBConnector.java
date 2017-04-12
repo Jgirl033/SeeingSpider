12c414569f29a1b9874477427ac2814cc1a08ff0
Jgirl033 <18819423747@163.com>
2017-04-12 10:55:26 +0800

    Created the abstract classes which aim at analysing the source code and get information from the source code.

---
+++ src/common/util/DBConnector.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package common.util;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author admin
+ */
+public abstract class DBConnector {
+    public abstract Connection getDBConnection();
+}
