0b6e1ca9ca8a1d74d1cdfeaa36792c450a7677d4
Jgirl033 <18819423747@163.com>
2017-04-13 11:46:50 +0800

    feat(DAL): add method to update databse when url is sent to client.
    The server program use database to record the url. If client request new task, do as follow:
    - Update the field is_downloading 1.
    - Insert the client's IP into the field IP.
    
    BREAKING CHANGE: add new method.

--- src/common/util/DBOperator.java
+++ src/common/util/DBOperator.java
@@ -8,17 +8,40 @@ package common.util;
 import common.entity.Comment;
 import common.entity.Movie;
 import common.entity.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author admin
  */
 public abstract class DBOperator {
+    
     public abstract boolean isUserExist(String uid);
+    
     public abstract boolean isMovieExist(String mid);
+    
     public abstract boolean isCommentExist(String uid, String mid);
+    
     public abstract void saveMovie(Movie m);
+    
     public abstract void saveComment(ArrayList<Comment> commentList);
+    
     public abstract void saveUser(ArrayList<User> userList);
+    
+    public static void updateDatabase(String IP) {
+        Connection dbc = new DBConnector().getDBConnection();
+        String sql = "UPDATE task SET is_downloading =0 WHERE is_downloaded =0 and IP=?";
+        try {
+            PreparedStatement ps = dbc.prepareStatement(sql);
+            ps.setString(0, IP);
+            ps.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBOperator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
