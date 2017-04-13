912816c987a17d37b56cdc760e091a28b0430361
Jgirl033 <18819423747@163.com>
2017-04-13 11:37:27 +0800

    Refactored the abstract class and made it a non-abstract class.

--- src/common/util/DBConnector.java
+++ src/common/util/DBConnector.java
@@ -6,11 +6,42 @@
 package common.util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
+ * 数据库连接类
  *
  * @author admin
  */
-public abstract class DBConnector {
-    public abstract Connection getDBConnection();
+public class DBConnector{
+
+    private Connection conn;
+
+    public DBConnector() {
+        try {
+            //加载驱动，这一句也可写为：Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/spider?characterEncoding=utf-8", "root", "iiip");
+            System.out.println("连接成功！");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            System.out.println("混蛋 : " + ex.toString());
+            System.out.println("连接失败！");
+        }
+    }
+
+    public DBConnector(String str, String user, String password) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            this.conn = DriverManager.getConnection(str, user, password);
+            System.out.println("连接成功！");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            System.out.println("Error : " + ex.toString());
+            System.out.println("连接失败！");
+        }
+    }
+
+    public Connection getDBConnection() {
+        return this.conn;
+    }
 }
